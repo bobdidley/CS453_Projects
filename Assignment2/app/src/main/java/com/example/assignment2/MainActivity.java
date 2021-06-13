ccbdfb79e2d61f528d42ad66547eec2929f55ba1
Fiona0131 <60491803+Fiona0131@users.noreply.github.com>
2021-06-12 22:19:17 -0700

    Slide Show

--- Assignment2/app/src/main/java/com/example/assignment2/MainActivity.java
+++ Assignment2/app/src/main/java/com/example/assignment2/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.ViewFlipper;
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     private ViewFlipper simpleViewFlipper;
     ImageView imageView;
     private Button btnPrev, btnNext;
+    private CheckBox slideview;
     int i =0;
     int animals[] = {R.drawable.animal13, R.drawable.animal14, R.drawable.animal15,
             R.drawable.animal16, R.drawable.animal17, R.drawable.animal18};
@@ -23,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
         simpleViewFlipper = (ViewFlipper) findViewById(R.id.viewFlipper);
         btnPrev = (Button)findViewById(R.id.btnPrevious);
         btnNext = (Button)findViewById(R.id.btnNext);
+        slideview =(CheckBox)findViewById(R.id.chkBoxShow);
+
 
         //
         for(int i=0; i<animals.length;i++){
@@ -48,7 +52,35 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Slide View Option
+       slideview.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               // when the "Slide Show" checkbox is checked,
+               // the slide show runs
+               if(slideview.isChecked()){
+                   simpleViewFlipper.setAutoStart(true);
+                   simpleViewFlipper.setFlipInterval(3000);
+                   simpleViewFlipper.startFlipping();
+               }
+               // When the "Slide show" checkbox is unchecked
+               // the slide show ends
+               else{
+                   simpleViewFlipper.stopFlipping();
+               }
+
+           }
+        });
+
+
 
     }
+
+
+
+
+
+
+
 
 }
