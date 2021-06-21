5c000fb92ff47ca084e04965eea1a5999b840483
bobdidley <james.austin@student.csulb.edu>
2021-06-20 18:19:35 -0700

    Mock data for spinners

--- Assignment3/app/src/main/java/com/example/project3/MainActivity.java
+++ Assignment3/app/src/main/java/com/example/project3/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.project3;
 
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -13,20 +16,52 @@ import com.example.project3.content.Vehicles;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener  {
 
     private boolean mTwoPane = false;
+    private String[] makes = {"", "Car1", "Car2"};
+    private String[] models = {"", "Mod1", "Mod2"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vehicle_list);
 
+        Spinner spin_make = findViewById(R.id.spinner_make);
+        spin_make.setOnItemSelectedListener(this);
+        Spinner spin_model = findViewById(R.id.spinner_model);
+        spin_model.setOnItemSelectedListener(this);
+
+        ArrayAdapter<String> aa_makes = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, makes);
+        spin_make.setAdapter(aa_makes);
+        ArrayAdapter<String> aa_models = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, models);
+        spin_model.setAdapter(aa_models);
 
         RecyclerView rv = findViewById(R.id.list);
         rv.setAdapter(new SimpleItemRecyclerViewAdapter(Vehicles.VEHICLE_ITEMS));
 
         // is the container layout available? If so, set mTwoPane to true
+
+    }
+
+    /**
+     *
+     * @param parent
+     * @param view
+     * @param position
+     * @param id
+     */
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    /**
+     *
+     * @param parent
+     */
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 
