d9ce7e42d053165ed9e6e1261c367bc7eb2b8c97
Fiona0131 <60491803+Fiona0131@users.noreply.github.com>
2021-06-28 14:24:43 -0700

    Basic Signup design

--- FinalProject/app/src/main/java/com/example/todolist/Login.java
+++ FinalProject/app/src/main/java/com/example/todolist/Login.java
@@ -1,15 +1,36 @@
 package com.example.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    private Button btn_signup;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        getSupportActionBar().hide();
+        btn_signup = findViewById(R.id.btnSignup);
+
+        btn_signup.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // Intent signup --> Redirect to signup page after clicking signup Button
+                Intent signup = new Intent(getApplicationContext(), Signup.class);
+                startActivity(signup);
+            }
+        });
+
+
+
+
     }
 }
