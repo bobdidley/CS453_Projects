5b5a9c199ee1148e84185f7a3157108c2de70e02
Fiona0131 <60491803+Fiona0131@users.noreply.github.com>
2021-06-29 13:46:53 -0700

    Update ChangePasswordActivity.java

--- FinalProject/app/src/main/java/com/example/todolist/ChangePasswordActivity.java
+++ FinalProject/app/src/main/java/com/example/todolist/ChangePasswordActivity.java
@@ -5,13 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class ChangePasswordActivity extends AppCompatActivity {
+
+    private Button btn_update;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_password);
+
+        btn_update = findViewById(R.id.btnUpdate);
+
+        btn_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent update = new Intent(getApplicationContext(), ProfileActivity.class);
+                startActivity(update);
+                Toast.makeText(getApplicationContext(),"New password is updated", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+
+
 
     }
 }
