928eddf2bbbecdd580dd182c2c0548ee40ab289f
Fiona0131 <60491803+Fiona0131@users.noreply.github.com>
2021-06-30 00:17:31 -0700

    update

--- FinalProject/app/src/main/java/com/example/todolist/TaskActivity.java
+++ FinalProject/app/src/main/java/com/example/todolist/TaskActivity.java
@@ -16,6 +16,14 @@ public class TaskActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task);
 
+        addTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent addTask = new Intent(TaskActivity.this, AddTaskActivity.class);
+                startActivity(addTask);
+            }
+        });
+
 
     }
 }
