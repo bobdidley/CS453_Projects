eb8391555b7276a33881d10103136d1f063c112b
Fiona0131 <60491803+Fiona0131@users.noreply.github.com>
2021-06-30 16:33:48 -0700

    update

--- FinalProject/app/src/main/java/com/example/todolist/TaskActivity.java
+++ FinalProject/app/src/main/java/com/example/todolist/TaskActivity.java
@@ -1,20 +1,35 @@
 package com.example.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class TaskActivity extends AppCompatActivity {
 
     private Button addTask;
+    private RecyclerView taskRecyclerView;
+    private CustomTaskAdapter taskAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task);
+        getSupportActionBar().hide();
+
+
+        //
+        Intent intent = getIntent();
+        HashMap<String,String> task_details = (HashMap<String, String>) intent.getSerializableExtra("detail");
+
 
         addTask.setOnClickListener(new View.OnClickListener() {
             @Override
