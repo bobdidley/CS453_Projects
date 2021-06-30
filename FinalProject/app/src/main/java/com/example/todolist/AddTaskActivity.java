d6f0d7471b41ed7cee0f2d2eaac65729f811ee3e
bobdidley <james.austin@student.csulb.edu>
2021-06-29 23:22:24 -0700

    Create Task all fields are set

--- FinalProject/app/src/main/java/com/example/todolist/AddTaskActivity.java
+++ FinalProject/app/src/main/java/com/example/todolist/AddTaskActivity.java
@@ -1,5 +1,6 @@
 package com.example.todolist;
 
+import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.util.Log;
 import android.widget.*;
@@ -12,7 +13,9 @@ import android.view.View;
 
 import com.example.todolist.db_contents.Task;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Locale;
 
 public class AddTaskActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
@@ -21,8 +24,7 @@ public class AddTaskActivity extends AppCompatActivity implements AdapterView.On
     private EditText task_category;
     private Spinner task_priority;
     private Spinner task_time;
-//    private DatePicker datePicker;
-//    private Calendar calendar;
+    private Button task_date;
 
     private final String[] times = {"Select a time", "12:00 AM", "1:00 AM", "2:00 AM", "3:00 AM", "4:00 AM", "5:00 AM", "6:00 AM",
             "7:00 AM", "8:00 AM", "9:00 AM", "10:00 AM", "11:00 AM","12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM",
@@ -47,22 +49,38 @@ public class AddTaskActivity extends AppCompatActivity implements AdapterView.On
         task_category = findViewById(R.id.txtTaskCategory);
         task_priority = findViewById(R.id.spinnerPriority);
         task_time = findViewById(R.id.spinnerTime);
-
+        task_date = findViewById(R.id.btnDatePicker);
         addTask = findViewById(R.id.btnAddTask);
 
+        // set spinner options with adapters
         task_priority.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, priorities));
         task_time.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, times));
+
+        task_date.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 task_date.setBackgroundColor(getColor(R.color.lightslategray));
+                 showDialog(999);
+                 new DatePickerDialog(AddTaskActivity.this, dateListener, calendar.get(Calendar.YEAR),
+                         calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
+             }
+         });
 
         addTask.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 name = task_name.getText().toString();
                 category = task_category.getText().toString();
+                date = task_date.getText().toString();
+
+                // debug
+                Log.i("Date", "task_date = " + getString(R.string.taskDate) + "\nDate = " + date);
 
                 if(name.equals("")) { task_name.setError("Missing field"); }
                 if(category.equals("")) { task_category.setError("Missing field"); }
                 boolean pryCheck = true;
                 boolean timeCheck = true;
+                boolean dateCheck = true;
                 if(task_priority.getSelectedItem().equals(priorities[0])) {
                     ((TextView) task_priority.getSelectedView()).setError(priorities[0]);
                     pryCheck = false;
@@ -71,8 +89,12 @@ public class AddTaskActivity extends AppCompatActivity implements AdapterView.On
                     ((TextView) task_time.getSelectedView()).setError(times[0]);
                     timeCheck = false;
                 }
+                if(date.equals(getString(R.string.taskDate))) {
+                    task_date.setBackgroundColor(getColor(R.color.design_default_color_error));
+                    dateCheck = false;
+                }
 
-                if(task_name.getError() == null && task_category.getError() == null && pryCheck && timeCheck) {
+                if(task_name.getError() == null && task_category.getError() == null && pryCheck && timeCheck && dateCheck) {
                     Intent addTask = new Intent(AddTaskActivity.this, ProfileActivity.class);
                     startActivity(addTask);
                 } else {
@@ -112,4 +134,26 @@ public class AddTaskActivity extends AppCompatActivity implements AdapterView.On
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {}
+
+    private Calendar calendar = Calendar.getInstance();
+
+    private DatePickerDialog.OnDateSetListener dateListener = new
+            DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view,
+                                      int year, int month, int day) {
+                    // TODO Auto-generated method stub
+                    calendar.set(Calendar.YEAR, year);
+                    calendar.set(Calendar.MONTH, month);
+                    calendar.set(Calendar.DAY_OF_MONTH, day);
+                    updateDate();
+                }
+            };
+
+    private void updateDate() {
+        String myFormat = "MM/dd/yyyy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.ENGLISH);
+
+        task_date.setText(sdf.format(calendar.getTime()));
+    }
 }
