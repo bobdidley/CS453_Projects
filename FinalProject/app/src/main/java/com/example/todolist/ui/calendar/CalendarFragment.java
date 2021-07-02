3068cc051750a65c178d30878504a9d80a04fa6c
bobdidley <james.austin@student.csulb.edu>
2021-07-01 17:57:24 -0700

    Calendar select date

--- FinalProject/app/src/main/java/com/example/todolist/ui/calendar/CalendarFragment.java
+++ FinalProject/app/src/main/java/com/example/todolist/ui/calendar/CalendarFragment.java
@@ -4,42 +4,32 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 import android.widget.TextView;
 
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
-import com.example.todolist.databinding.FragmentCalendarBinding;
+import com.example.todolist.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class CalendarFragment extends Fragment {
 
-    private CalendarViewModel slideshowViewModel;
-    private FragmentCalendarBinding binding;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        slideshowViewModel =
-                new ViewModelProvider(this).get(CalendarViewModel.class);
+        View view = inflater.inflate(R.layout.fragment_calendar, container, false);
 
-        binding = FragmentCalendarBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        CalendarView calendar = view.findViewById(R.id.calendarView);
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+        String selectedDate = sdf.format(new Date(calendar.getDate()));
 
-//        final TextView textView = binding.textSlideshow;
-//        slideshowViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
-        return root;
-    }
+        Toast.makeText(getContext(), selectedDate, Toast.LENGTH_SHORT).show();
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+        return view;
     }
 }
